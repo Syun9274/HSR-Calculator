29cc31a3b50c202f748007cd1fd52e8ad9a1ef4f
YSMin <zxczxv7624@gmail.com>
2025-09-05 03:52:49 +0900

    init

---
+++ src/main/java/com/github/syun9274/hsr_damage_calculator/HsrDamageCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.syun9274.hsr_damage_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsrDamageCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsrDamageCalculatorApplication.class, args);
+	}
+
+}

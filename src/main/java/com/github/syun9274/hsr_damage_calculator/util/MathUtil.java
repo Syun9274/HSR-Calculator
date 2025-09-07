e02b6ddff4230ec3d1808b7f555066389e53330f
YSMin <zxczxv7624@gmail.com>
2025-09-08 03:02:32 +0900

    [#1]feat: 계산 결과 최종 표기 방식 구현
    - 스탯의 경우 반올림
    - 데미지의 경우 올림
    - 추후 최종 확인 필요

--- src/main/java/com/github/syun9274/hsr_damage_calculator/util/MathUtil.java
+++ src/main/java/com/github/syun9274/hsr_damage_calculator/util/MathUtil.java
@@ -4,4 +4,24 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class MathUtil {
+
+    /**
+     * 스탯 표기 방식
+     * - 반올림 처리로 추정
+     * @param value 스탯
+     * @return 정수 변환 스탯
+     */
+    public static int toGameStatInt(double value) {
+        return (int) Math.round(value);
+    }
+
+    /**
+     * 데미지 표기 방식
+     * - 올림 처리로 추정
+     * @param value 데미지
+     * @return 정수 변환 데미지
+     */
+    public static int toGameDamageInt(double value) {
+        return (int) Math.ceil(value);
+    }
 }

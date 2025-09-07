59c6143de2e9ddbd8e20cc499905197bd9ce2cb9
YSMin <zxczxv7624@gmail.com>
2025-09-08 03:01:47 +0900

    [#1]feat: 계산 공식 상수 작성

--- src/main/java/com/github/syun9274/hsr_damage_calculator/calculator/formula/DamageFormula.java
+++ src/main/java/com/github/syun9274/hsr_damage_calculator/calculator/formula/DamageFormula.java
@@ -1,8 +1,17 @@
 package com.github.syun9274.hsr_damage_calculator.calculator.formula;
 
-// 데미지 계산 공식 상수
 public class DamageFormula {
 
+    // 저항 기본값
+    public static final double WEAKNESS_RESISTANCE = 0.0; // 약점 0%
+    public static final double NORMAL_RESISTANCE = 0.2; // 일반 20%
+    public static final double STRONG_RESISTANCE = 0.4; // 정예 40%
+
+    // 저항 한계값
+    public static final double MIN_RESISTANCE = -1.0; // -100%
+    public static final double MAX_RESISTANCE = 0.9; // 90%
+
+    // 강인성 피해 감소
     public static final double TOUGHNESS_DAMAGE_REDUCTION = 0.1; // 10%
 
 }

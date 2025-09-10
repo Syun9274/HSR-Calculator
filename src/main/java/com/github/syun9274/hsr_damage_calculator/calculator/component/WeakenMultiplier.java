b21a06a47906622d0abe065b817b53083f26527d
YSMin <zxczxv7624@gmail.com>
2025-09-11 00:35:23 +0900

    [#1]feat: 약화 버프 계산 적용

--- src/main/java/com/github/syun9274/hsr_damage_calculator/calculator/component/WeakenMultiplier.java
+++ src/main/java/com/github/syun9274/hsr_damage_calculator/calculator/component/WeakenMultiplier.java
@@ -1,12 +1,17 @@
 package com.github.syun9274.hsr_damage_calculator.calculator.component;
 
+import com.github.syun9274.hsr_damage_calculator.model.Buff;
+import com.github.syun9274.hsr_damage_calculator.model.enums.BuffType;
+import com.github.syun9274.hsr_damage_calculator.util.MathUtil;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class WeakenMultiplier {
 
     /*
-    적군 스스로 약화 디버프 (현재로서는 없음, 무시 가능)
+    적군 스스로 약화 디버프 (나타샤나 삼포의 특성, 또는 적 스스로 약화)
 
     The Weaken Multiplier is related to the Weaken status effect
     that can be applied by Natasha and Sampo’s passive Trace.
@@ -15,5 +20,11 @@ public class WeakenMultiplier {
     Weaken Multiplier = 100% - Weaken%
      */
 
+    public double getWeakenMultiplier(List<Buff> buffs) {
+        return 1 - calculateWeakenMultiplier(buffs);
+    }
 
+    private double calculateWeakenMultiplier(List<Buff> buffs) {
+        return MathUtil.sumPercentBuffs(buffs, BuffType.WEAKEN);
+    }
 }
